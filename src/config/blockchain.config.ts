c48d03478bb59cb36dfb4084b0b9000559a86743
OFUZOR CHUKWUEMEKE <uniben2018@gmail.com>
2025-11-11 18:38:32 +0100

    mainnet

--- src/config/blockchain.config.ts
+++ src/config/blockchain.config.ts
@@ -99,6 +99,6 @@ export function getDefaultSolanaNetwork(): BlockchainNetwork {
     case 'devnet':
     case 'solana-devnet':
     default:
-      return BlockchainNetwork.SOLANA_DEVNET;
+      return BlockchainNetwork.SOLANA_MAINNET;
   }
 }
